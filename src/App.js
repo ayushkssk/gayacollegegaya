caab91abb8b9b798a57e6fa13af1728a8238c57f
Ayush Singh <ayushkssk@gmail.com>
2024-11-17 13:12:13 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
+      <Routes>
+        <Route path="/pages/login" element={<Login />} />
+        <Route path="/pages/register" element={<Register />} />
+      </Routes>
     </Router>
   );
 };
